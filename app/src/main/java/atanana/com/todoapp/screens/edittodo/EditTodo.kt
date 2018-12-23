f78587e3c0d132b90c55cdecbd1f0cc8822cdaaf
Andrei <tanana@email.ru>
2018-12-23 15:49:49 +0300

    edit todo fragment init

---
+++ app/src/main/java/atanana/com/todoapp/screens/edittodo/EditTodo.kt
@@ -0,0 +1,23 @@
+package atanana.com.todoapp.screens.edittodo
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import atanana.com.todoapp.R
+
+class EditTodo : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_todo, container, false)
+    }
+
+
+}
