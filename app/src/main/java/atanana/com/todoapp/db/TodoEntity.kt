15a6eadce7010e8a3bf5ccc6235f41446eab1801
Andrei <tanana@email.ru>
2018-12-25 17:23:09 +0300

    add primary key

--- app/src/main/java/atanana/com/todoapp/db/TodoEntity.kt
+++ app/src/main/java/atanana/com/todoapp/db/TodoEntity.kt
@@ -1,9 +1,11 @@
 package atanana.com.todoapp.db
 
 import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 
 @Entity
 data class TodoEntity(
+    @PrimaryKey
     val id: Long? = null,
     val title: String = "",
     val text: String = ""
