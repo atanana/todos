81ff29b1c9f7580f2f7d055104b4715b2cfe6157
Andrei Tanana <andrei.tanana@deltatre.it>
2019-01-30 17:12:02 +0300

    update todos

--- app/src/main/java/atanana/com/todoapp/db/TodoDao.kt
+++ app/src/main/java/atanana/com/todoapp/db/TodoDao.kt
@@ -2,11 +2,12 @@ package atanana.com.todoapp.db
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 
 @Dao
 interface TodoDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(todo: TodoEntity): Long
 
     @Query("select * from todos")
