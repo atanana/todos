7ec93b0e79311fc7ad1a967fb2d283b025b16405
Andrei Tanana <andrei.tanana@deltatre.it>
2019-09-03 16:51:09 +0300

    firestore init

--- app/src/main/java/atanana/com/todoapp/data/Repository.kt
+++ app/src/main/java/atanana/com/todoapp/data/Repository.kt
@@ -3,6 +3,11 @@ package atanana.com.todoapp.data
 import atanana.com.todoapp.db.TodoEntity
 import atanana.com.todoapp.db.TodosDatabase
 import atanana.com.todoapp.db.toEntity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlin.coroutines.suspendCoroutine
 
 class Repository(database: TodosDatabase) {
     private val todoDao = database.todosDao()
@@ -12,7 +17,15 @@ class Repository(database: TodosDatabase) {
 
     suspend fun add(todo: Todo) {
         todoDao.insert(todo.toEntity())
+        Firebase.firestore.collection("todos")
+            .add(todo)
+            .await()
     }
 
     suspend fun byId(id: Long): Todo? = todoDao.byId(id)?.toTodo()
+}
+
+suspend fun Task<DocumentReference>.await() = suspendCoroutine<Unit> { continuation ->
+    addOnSuccessListener { continuation.resumeWith(Result.success(Unit)) }
+    addOnFailureListener { error -> continuation.resumeWith(Result.failure(error)) }
 }
